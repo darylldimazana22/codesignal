686ff051296f39076347e5b6319949ba9e1bc65c
Daryll Dimazana <darylldimazana22@gmail.com>
2019-01-05 04:02:09 +0800

    Create reverseParentheses.js

---
+++ Arcade/reverseParentheses.js
@@ -0,0 +1,11 @@
+function reverseParentheses(s) {
+  while (s.indexOf(")") != -1) {
+    var c = s.indexOf(")");
+    var a = s.lastIndexOf('(', c);
+    
+    var b = s.slice(a + 1, c).split("").reverse().join("");
+    var s = s.slice(0, a) +b + s.slice(c + 1); 
+  }
+  
+  return s;
+}
